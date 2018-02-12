a4afd5cf06107c61d3b917e67c64b8b9a87fba0f
Mitesh P <miteshp030@gmail.com>
2018-02-12 20:20:16 +0400

    First time

---
+++ src/test/java/GIT/GITTest/BaseClass.java
@@ -0,0 +1,22 @@
+package GIT.GITTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	@Test
+	public void init()
+	{
+		    String URL = "http://ae.akbartravels.com";
+		    WebDriver driver;
+		    
+		    System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+	        driver = new ChromeDriver();
+	        driver.get(URL);
+		    
+	}
+
+}
